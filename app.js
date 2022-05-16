78ad90211cf39b784f80fbeb3e2f2ae965cde18d
Rob <rsamoraj11@gmail.com>
2022-05-16 18:56:15 -0500

    Add method of counting points in app.js

--- app.js
+++ app.js
@@ -17,23 +17,37 @@ function userPlay() {
 
 function playRound(playerSelection, computerSelection) {
 
+    let winner = '';
+    let roundMessage = '';
+
+
     if (computerSelection === playerSelection) {
-        return 'It\'s a draw!'
+        roundMessage = 'It\'s a draw!'
     } else if (computerSelection === 'rock' && playerSelection === 'paper') {
-        return 'You win! Paper beats rock.'
+        roundMessage = 'You win! Paper beats rock.'
+        winner = 'player'
     } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
-        return 'You win! Scissors beats paper.'
+        roundMessage = 'You win! Scissors beats paper.'
+        winner = 'player';
     } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
-        return 'You win! Rock beats scissors.'
+        roundMessage = 'You win! Rock beats scissors.'
+        winner = 'player';
     } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-        return 'You lose! Paper beats rock.'
+        roundMessage = 'You lose! Paper beats rock.'
+        winner = 'computer'
     } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
-        return 'You lose! Rock beats scissors.'
+        roundMessage = 'You lose! Rock beats scissors.'
+        winner = 'computer'
     } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-        return 'You lose! Scissors beats paper.'
+        roundMessage = 'You lose! Scissors beats paper.'
+        winner = 'computer'
     }
+
+
+    return winner;
 }
 
+/* 
 const playerSelection = userPlay();
 const computerSelection = computerPlay();
 let result = playRound(playerSelection, computerSelection);
@@ -41,3 +55,38 @@ let result = playRound(playerSelection, computerSelection);
 console.log('Your choice: ', playerSelection);
 console.log('Computer\'s choice: ', computerSelection);
 console.log('Result is: ', result);
+*/
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let gameWinner = '';
+
+    for (let i = 0; i < 5; i++) {
+        let roundWinner = playRound(userPlay(), computerPlay());
+
+        if (roundWinner === 'player') {
+            playerScore++;
+        } else if (roundWinner === 'computer') {
+            computerScore++;
+        }
+    }
+
+    if (playerScore > computerScore) {
+        gameWinner = 'Player';
+    } else if (computerScore > playerScore) {
+        gameWinner = 'Computer';
+    } else {
+        gameWinner = 'Draw';
+    }
+
+    console.log(`Player: ${playerScore} | Computer: ${computerScore}`);
+
+    if (gameWinner === 'Draw') {
+        console.log(`No winner -- draw!`);
+    } else {
+        console.log(`Match winner: ${gameWinner}`);
+    }
+}
+
+game();
